6355d12e556e47ebff80ecff5c80171d66f5aac6
TolasaGanati <tolog766@gmail.com>
2024-09-06 17:08:39 +0300

    some change for deployment

--- index.js
+++ index.js
@@ -5,17 +5,29 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 10000;
-const songRouter = require('./routes/songs'); 
+const songRouter = require('./routes/songs');
 
 // Middleware
 app.use(cors({
-  origin: 'https://music-app-front-end-swart.vercel.app/', 
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      'http://localhost:5173', // Development URL
+      'https://music-app-front-end-swart.vercel.app', // Production URL
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
+
+
 app.use(express.json());
 
 // Routes
-app.use('/api/songs', songRouter); 
+app.use('/api/songs', songRouter);
 
 // Root route to handle GET requests to '/'
 app.get('/', (req, res) => {
@@ -23,14 +35,13 @@ app.get('/', (req, res) => {
 });
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Connection error:', err);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Connection error:', err);
+  });
 
 // Start the server
 app.listen(PORT, () => {
--- routes/songs.js
+++ routes/songs.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const Song = require('../models/Song');
 const router = express.Router();
 
