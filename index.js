f11e698cb2474cf49f69186c9ec16c4ccf64022f
TolasaGanati <99481807+TolasaGanati@users.noreply.github.com>
2024-09-07 10:30:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const songRouter = require('./routes/songs');
 app.use(cors({
   origin: (origin, callback) => {
     const allowedOrigins = [
-      'http://localhost:5173', // Development URL
+      //'http://localhost:5173', // Development URL
       'https://music-app-front-end-5iqg-l3hrboh4j-tolasaganatis-projects.vercel.app/', // Production URL
     ];
     if (!origin || allowedOrigins.includes(origin)) {
